f2d3d621ba90757a6372543aaacd590900b2b8a5
InsanelyDeveloper <insanelydeveloper@gmail.com>
2020-06-12 14:16:29 -0400

    Forgot to update this one

--- ExampleProject/Versions/Common/src/main/java/me/insanely/version/example/common/base/MinecraftVersion.java
+++ ExampleProject/Versions/Common/src/main/java/me/insanely/version/example/common/base/MinecraftVersion.java
@@ -3,6 +3,5 @@ package me.insanely.version.example.common.base;
 public enum MinecraftVersion {
 
     v1_8_R3,
-    v1_9_R2
 
 }
--- ExampleProject/Versions/Common/src/main/java/me/insanely/version/example/common/base/json/JsonModulesLoaderImpl.java
+++ ExampleProject/Versions/Common/src/main/java/me/insanely/version/example/common/base/json/JsonModulesLoaderImpl.java
@@ -9,10 +9,13 @@ import org.jetbrains.annotations.NotNull;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JsonModulesLoaderImpl implements JsonModulesLoader {
 
     @NotNull private final DefaultJsonModuleClassLoader defaultClassLoader = new DefaultJsonModuleClassLoader();
+    @NotNull private final Map<Class<?>, JsonModuleClassLoader> classLoaders = new HashMap<>();
 
     @Override
     public void load(@NotNull VersionProviderRegistry registry, @NotNull MinecraftVersion version, @NotNull ObjectMapper objectMapper, @NotNull InputStream jsonInputStream)
@@ -20,18 +23,31 @@ public class JsonModulesLoaderImpl implements JsonModulesLoader {
         JsonVersionModule[] modulesArray = objectMapper.readValue(jsonInputStream, JsonVersionModule[].class);
         for (JsonVersionModule jsonModule : modulesArray) {
             if (jsonModule.getVersion() == version) {
+                Class<?> moduleClass = Class.forName(jsonModule.getClassPath());
                 if (jsonModule.getLoaderClassPath() == null) {
-                    VersionModule versionModule = this.defaultClassLoader.loadFromClass(Class.forName(jsonModule.getClassPath()));
+                    VersionModule versionModule = this.defaultClassLoader.loadFromClass(moduleClass);
                     versionModule.configure(registry);
                 } else {
-                    if (!JsonModuleClassLoader.class.isAssignableFrom(Class.forName(jsonModule.getLoaderClassPath()))) {
+                    Class<?> loaderClass = Class.forName(jsonModule.getLoaderClassPath());
+                    if (!JsonModuleClassLoader.class.isAssignableFrom(loaderClass)) {
                         throw new IllegalArgumentException(jsonModule.getLoaderClassPath() + " is not a class loader");
                     }
-                    JsonModuleClassLoader classLoader = (JsonModuleClassLoader) Class.forName(jsonModule.getLoaderClassPath()).newInstance();
+                    JsonModuleClassLoader classLoader;
+                    if (this.classLoaders.containsKey(loaderClass)) {
+                        classLoader = this.classLoaders.get(loaderClass);
+                    } else {
+                        classLoader = (JsonModuleClassLoader) loaderClass.newInstance();
+                        this.classLoaders.put(loaderClass, classLoader);
+                    }
                     VersionModule versionModule = classLoader.loadFromClass(Class.forName(jsonModule.getClassPath()));
                     versionModule.configure(registry);
                 }
             }
         }
+    }
+
+    @Override
+    public void registerModuleClassLoader(@NotNull JsonModuleClassLoader classLoader) {
+        this.classLoaders.put(classLoader.getClass(), classLoader);
     }
 }
